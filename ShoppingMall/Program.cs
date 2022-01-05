56f062c239fb886d3999da631692fc360d9a2164
dannyLiu <h152074716@gmail.com>
2022-01-05 17:34:24 +0800

    建立後台產品區域

--- ShoppingMall/Program.cs
+++ ShoppingMall/Program.cs
@@ -27,6 +27,10 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.MapControllerRoute(
+        name: "MyArea",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 
 app.MapControllerRoute(
     name: "default",
