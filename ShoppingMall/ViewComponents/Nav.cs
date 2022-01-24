06f0e052b3e1b9f0b7bacc04875b132914bfb70c
dannyLiu <h152074716@gmail.com>
2022-01-25 00:55:22 +0800

    建立 Nav  檢視元件

---
+++ ShoppingMall/ViewComponents/Nav.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingMall.Services;
+
+namespace ShoppingMall.ViewComponents
+{
+    public class Nav : ViewComponent
+
+    {
+        private readonly ICatalogService _catalogService;
+
+        public Nav(ICatalogService catalogService)
+        {
+            _catalogService = catalogService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = await _catalogService.GetAllAsync();
+            return View(items);
+        }
+    }
+}
