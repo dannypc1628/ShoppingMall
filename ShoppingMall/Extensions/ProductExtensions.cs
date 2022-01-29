46420a4b0d56d41a995858a964ab5c51b5f80c27
dannyLiu <h152074716@gmail.com>
2022-01-29 23:15:44 +0800

    建立 ProductViewModel 與 ProductExtensions

---
+++ ShoppingMall/Extensions/ProductExtensions.cs
@@ -0,0 +1,21 @@
+﻿using ShoppingMall.Models;
+using ShoppingMall.Models.ViewModels;
+
+namespace ShoppingMall.Extensions
+{
+    public static class ProductExtensions
+    {
+        public static Product ToProduct(this ProductViewModel productViewModel)
+        {
+            return new Product()
+            {
+                CatalogId = productViewModel.CatalogId,
+                Count = productViewModel.Count,
+                Description = productViewModel.Description,
+                Name = productViewModel.Name,
+                Summary = productViewModel.Summary,
+                UnitPrice = productViewModel.UnitPrice,
+            };
+        }
+    }
+}
---
+++ ShoppingMall/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ShoppingMall.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Count { get; set; }
+        public int CatalogId { get; set; }
+
+        public IFormFile Picture { get; set; }
+
+    }
+}
