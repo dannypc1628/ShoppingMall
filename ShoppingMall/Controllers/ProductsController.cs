d6ebfb3086a8682fc293a3e116edf1c2fb8c938e
dannyLiu <h152074716@gmail.com>
2022-02-03 02:08:10 +0800

    加入顧客用 Product 的 Controller 、 View 樣板

---
+++ ShoppingMall/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingMall.Models;
+
+namespace ShoppingMall.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ShoppingmallContext _context;
+
+        public ProductsController(ShoppingmallContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            var shoppingmallContext = _context.Product.Include(p => p.Catalog);
+            return View(await shoppingmallContext.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .Include(p => p.Catalog)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+    }
+}
