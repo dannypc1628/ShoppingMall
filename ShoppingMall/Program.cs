23c03ca87a22188b6f3c90f44ef826f4b97cc320
dannyLiu <h152074716@gmail.com>
2022-02-09 23:03:49 +0800

    首頁改為 ProductsController

--- ShoppingMall/Program.cs
+++ ShoppingMall/Program.cs
@@ -40,6 +40,6 @@ app.MapControllerRoute(
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Products}/{action=Index}/{id?}");
 
 app.Run();
