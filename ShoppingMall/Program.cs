a9656314723b3f7958d735a2a1bd98aeee5462eb
dannyLiu <h152074716@gmail.com>
2022-02-10 17:52:39 +0800

    在 Console 紀錄中顯示 EF Core 產生的 SQL 語法

--- ShoppingMall/Program.cs
+++ ShoppingMall/Program.cs
@@ -8,9 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ShoppingmallContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolContext"), contextOptionsBuilder =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolContext"), contextOptionsBuilder =>
             {
                 contextOptionsBuilder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+                options.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Name }, LogLevel.Information);
             }));
 
 builder.Services.AddTransient<ICatalogService, CatalogService>();
